4d6955258bf96b3c9ce04ef48e8882ba063e4cb0
Develper Bishal Thapa <90990481+devbith@users.noreply.github.com>
2023-05-28 18:47:06 +0100

    Create setup_script.sh

---
+++ setup_script.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Step 1: Install Quicksilver
+brew install --cask quicksilver
+
+# Step 2: Create the script directory if it doesn't exist
+script_dir="$HOME/.vim/venkatway"
+mkdir -p "$script_dir"
+
+# Step 3: Create the script file
+cat <<EOF > "$script_dir/script.sh"
+#!/bin/bash
+
+j_directory_path=\$(echo \${1} | awk -F '/' -v OFS='/' '\$NF=""; print \$0')
+j_file=\$(echo \${1} | awk -F '/' '{print \$NF}' | sed 's/.java//')
+script_dir=\$(cd "\$(dirname "\${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+# Compile 
+output=\$(javac -cp ".:\${j_directory_path}/*" \${1} 2>&1) 
+
+# Run if compiled
+if [ \$? -eq 0 ]; then
+  output=\$(java -cp ".:\${j_directory_path}/" \${j_file} 2>&1)
+fi
+
+escaped_output=\$(printf "%s" "\$output" | sed 's/"/\\"/g')
+osascript -e "tell application \"Quicksilver\" to show large type \"\$escaped_output\""
+EOF
+
+# Step 4: Map the command in Vim
+echo 'noremap <f5> :silent !sh ~/.vim/venkatway/script.sh %:p &<CR>:redraw!<CR>' >> ~/.vimrc
+
+# Step 5: Display completion message
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
+echo -e "\n\n\tMapped <F5> key as command in ${YELLOW}Vim${NC} to run the script ~/.vim/venkatway/script.sh"
+
+echo -e "\n\tSetup completed. Your ${YELLOW}Vim${NC} is ready to run & view output of java program in a ${GREEN}Dr.Venkat Subramaniam${NC} style 😎."
